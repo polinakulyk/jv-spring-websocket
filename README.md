c3815a9303de18e2b000d335e6d7e3f4b0bccda1
Polina Kulyk <polina.kulyk@protonmail.com>
2020-08-05 21:02:15 +0300

    Initial Spring Boot setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# jv-spring-websocket
+Following the official Spring guide on using WebSocket to build an interactive web application. Source: https://spring.io/guides/gs/messaging-stomp-websocket/
---
+++ src/main/java/com/example/msgwebsocket/MsgWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.msgwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgWebsocketApplication.class, args);
+	}
+
+}
